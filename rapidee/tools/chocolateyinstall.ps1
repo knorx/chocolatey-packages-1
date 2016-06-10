d3bff874f9f969848893b72d4e53b6f296785029
Raphx <rcxng93@hotmail.com>
2016-06-11 02:09:08 +0800

    Add original rapidee package

---
+++ rapidee/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = "rapidee"
+$installDir = Join-Path -Path $env:ChocolateyInstall -ChildPath "lib\$packagename\tools"
+$url = "http://www.rapidee.com/download/RapidEE.zip"
+$url64 = "http://www.rapidee.com/download/RapidEEx64.zip"
+$executable = Join-Path -Path $installDir -ChildPath "RapidEE.exe"
+
+Install-ChocolateyZipPackage -packageName $packageName -url $url -url64bit $url64 -UnzipLocation $installDir 
+New-Item -Path ($executable + ".gui") -Force -ErrorAction SilentlyContinue
+
+$startMenu = [Environment]::GetFolderPath("CommonPrograms")
+$startMenuLink = Join-Path -Path $startMenu -childPath "Rapid Environment Editor.lnk"
+try { Install-ChocolateyShortcut -shortcutFilePath  "$startMenuLink" -targetPath "$executable" } finally {} #don't die on shortcut creation
+
