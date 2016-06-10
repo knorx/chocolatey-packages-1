297583a2964f1673e02c78ec9b0b21be1002ff9a
Raphx <rcxng93@hotmail.com>
2016-06-11 02:49:17 +0800

    Update rapidee package to 9.0.930

--- rapidee/tools/chocolateyinstall.ps1
+++ rapidee/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 $installDir = Join-Path -Path $env:ChocolateyInstall -ChildPath "lib\$packagename\tools"
 $url = "http://www.rapidee.com/download/RapidEE.zip"
 $url64 = "http://www.rapidee.com/download/RapidEEx64.zip"
-$executable = Join-Path -Path $installDir -ChildPath "RapidEE.exe"
+$executable = Join-Path -Path $installDir -ChildPath "rapidee.exe"
 
 Install-ChocolateyZipPackage -packageName $packageName -url $url -url64bit $url64 -UnzipLocation $installDir 
-New-Item -Path ($executable + ".gui") -Force -ErrorAction SilentlyContinue
 
 $startMenu = [Environment]::GetFolderPath("CommonPrograms")
-$startMenuLink = Join-Path -Path $startMenu -childPath "Rapid Environment Editor.lnk"
-try { Install-ChocolateyShortcut -shortcutFilePath  "$startMenuLink" -targetPath "$executable" } finally {} #don't die on shortcut creation
+$startMenuLink = Join-Path -Path $startMenu -ChildPath "Rapid Environment Editor.lnk"
+Install-ChocolateyShortcut -ShortcutFilePath "$startMenuLink" -TargetPath "$executable"
 
+New-Item -Path ($executable + ".gui") -Type file -Force -ErrorAction SilentlyContinue
